1351872aca81328c0149de7e7a0772303a7138e2
rujool11 <132924872+rujool11@users.noreply.github.com>
2025-10-21 13:19:28 +0530

    add post routes and controller signatures

---
+++ internal/controllers/posts.go
@@ -0,0 +1,27 @@
+package controllers
+
+import "github.com/gin-gonic/gin"
+
+func FetchAllPosts(c *gin.Context) {
+
+}
+
+func CreatePost(c *gin.Context) {
+
+}
+
+func GetPostById(c *gin.Context) {
+
+}
+
+func LikePost(c *gin.Context) {
+
+}
+
+func UnlikePost(c *gin.Context) {
+
+}
+
+func DeleteOwnPost(c *gin.Context) {
+
+}
